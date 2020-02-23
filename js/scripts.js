2889ffae047989c5dec966f8975703c8721da2ee
LaukeyMwaura <laukeymwaura@gmail.com>
2020-02-23 22:37:39 +0300

    classes and spana

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,34 @@
 $(document).ready(function() {
-    $("span").click(function() {
-      $(".showing").toggle();
-      $(".hidding").toggle();
+    $("span#design").click(function() {
+      $("showing1").toggle();
+      $(".hiding1").toggle();
     });
+});
+$(document).ready(function() {
+  $("span#dev").click(function() {
+    $(".showing2").toggle();
+    $(".hiding2").toggle();
+  });
+});
+$(document).ready(function() {
+  $("span#pro").click(function() {
+    $(".showing3").toggle();
+    $(".hiding3").toggle();
+  });
+});
+$(document).ready(function () {
+
+  $(".showing").hide();
+
+  $(".hiding").click(function(){
+       $("#design").toggle();
+  });
+
+  // $("#btn2").click(function(){
+  //      $("#p2").toggle();
+  // });
+
+  // $("#btn2").click(function(){
+  //      $("#p2").toggle();
+  // });
 });
