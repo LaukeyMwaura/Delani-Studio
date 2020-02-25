f31e71d8e77e3ab66d50bf3831464370f93f28a0
LaukeyMwaura <laukeymwaura@gmail.com>
2020-02-25 17:15:12 +0300

    The repetition of the document.ready removed

--- js/scripts.js
+++ js/scripts.js
@@ -3,32 +3,22 @@ $(document).ready(function () {
     $(".design").fadeOut();
     $(".showing1").fadeIn();
   });
-});
-$(document).ready(function () {
   $(".showing1").click(function () {
     $(".showing1").fadeOut();
     $(".design").fadeIn();
   });
-});
-$(document).ready(function () {
   $(".dev").click(function () {
     $(".dev").fadeOut();
     $(".showing2").fadeIn();
   });
-});
-$(document).ready(function () {
   $(".showing2").click(function () {
     $(".showing2").fadeOut();
     $(".dev").fadeIn();
   });
-});
-$(document).ready(function () {
   $(".pro").click(function () {
     $(".pro").fadeOut();
     $(".showing3").fadeIn();
   });
-});
-$(document).ready(function () {
   $(".showing3").click(function () {
     $(".showing3").fadeOut();
     $(".pro").fadeIn();
